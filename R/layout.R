a04317112ee701b9176713d2567fb62d53d083bb
Brian Lee Yung Rowe <r@zatonovo.com>
2013-07-10 00:00:00 +0000

    version 1.3.3

--- R/layout.R
+++ R/layout.R
@@ -1,3 +1,56 @@
+#' Manage layouts within the 'futile.logger' sub-system
+#' 
+#' Provides functions for managing layouts. Typically 'flog.layout' is only
+#' used when manually creating a logging configuration.
+#' 
+#' Layouts are responsible for formatting messages so they are human-readable.
+#' Like an appender, a layout is assigned to a logger by calling 'flog.layout'.
+#' The 'flog.layout' function is used internally to get the registered layout
+#' function. It is kept visible so user-level introspection is available.
+#' 
+#' flog.layout(name) \%::\% character : Function flog.layout(name='ROOT')
+#' 
+#' flog.layout(fn, name) \%::\% Function : character : Null
+#'
+#' flog.layout(fn, name='ROOT')
+#' 
+#' 'layout.simple' is a pre-defined layout function that prints messages in the
+#' following format: LEVEL [timestamp] Message. This is the default layout for
+#' the ROOT logger.
+#' 
+#' 'layout.format' allows you to specify the format string to use in printing a
+#' message. It is mostly included to provide an example of writing your own
+#' layout function.
+#' 
+#' @name flog.layout
+#' @aliases layout.simple layout.format
+#' @param \dots Used internally by lambda.r
+#' @return 'flog.layout' returns a layout function, which is wrapped in a
+#' parent function to enforce a consistent calling API.
+#' @author Brian Lee Yung Rowe
+#' @keywords data
+#' @examples
+#' 
+#' # Set the layout for 'my.package'
+#' flog.layout(layout.simple, name='my.package')
+#' 
+NULL
+
+# Get the layout for the given logger
+flog.layout(name) %::% character : Function
+flog.layout(name='ROOT') %as%
+{
+  logger <- flog.logger(name)
+  logger$layout
+}
+
+# Set the layout
+flog.layout(fn, name='ROOT') %as%
+{
+  flog.logger(name, layout=fn)
+  invisible()
+}
+
 # This file provides some standard formatters
 # This prints out a string in the following format:
 #   LEVEL [timestamp] message
@@ -8,16 +61,34 @@ layout.simple <- function(level, msg, ...)
   sprintf("%s [%s] %s\n", names(level),the.time, msg)
 }
 
+
 # This parses and prints a user-defined format string. Available tokens are
-# %l - Log level
-# %t - Timestamp
-# %m - Message
-# %f - Calling function
-# TODO: INCOMPLETE
-layout.format <- function(format)
+# ~l - Log level
+# ~t - Timestamp
+# ~n - Namespace
+# ~f - Calling function
+# ~m - Message
+#
+# layout <- layout.format('[~l] [~t] [~n.~f] ~m')
+# flog.layout(layout)
+layout.format <- function(format, datetime.fmt="%Y-%m-%d %H:%M:%S")
 {
+  where <- 1
   function(level, msg, ...) {
     if (! is.null(substitute(...))) msg <- sprintf(msg, ...)
+    the.level <- names(level)
+    the.time <- format(Sys.time(), datetime.fmt)
+    the.namespace <- get_namespace()
+    #print(sys.calls())
+    the.function <- tryCatch(sys.call(where)[[1]], error=function(e) "(shell)")
+    #pattern <- c('~l','~t','~n','~f','~m')
+    #replace <- c(the.level, the.time, the.namespace, the.function, msg)
+    message <- gsub('~l',the.level, format, fixed=TRUE)
+    message <- gsub('~t',the.time, message, fixed=TRUE)
+    message <- gsub('~n',the.namespace, message, fixed=TRUE)
+    message <- gsub('~f',the.function, message, fixed=TRUE)
+    message <- gsub('~m',msg, message, fixed=TRUE)
+    sprintf("%s\n", message)
   }
 }
 
--- R/logger.R
+++ R/logger.R
@@ -1,25 +1,115 @@
+#' Manage appenders within the 'futile.logger' sub-system
+#' 
+#' Provides functions for writing log messages and managing loggers. Typically
+#' only the flog.[trace|debug|info|warn|error|fatal] functions need to be used
+#' in conjunction with flog.threshold to interactively change the log level.
+#' 
+#' These functions represent the high level interface to futile.logger.
+#' 
+#' The primary use case for futile.logger is to write out log messages. There
+#' are log writers associated with all the predefined log levels: TRACE, DEBUG,
+#' INFO, WARN, ERROR, FATAL. Log messages will only be written if the log level
+#' is equal to or more urgent than the current threshold. By default the ROOT
+#' logger is set to INFO.
+#' 
+#' > flog.debug("This won't print") 
+#' 
+#' > flog.info("But this \%s", 'will') 
+#' 
+#' > flog.warn("As will \%s", 'this')
+#' 
+#' Typically, the built in log level constants are used in the call, which
+#' conform to the log4j levels (from least severe to most severe): TRACE,
+#' DEBUG, INFO, WARN, ERROR, FATAL. It is not a strict requirement to use these
+#' constants (any numeric value will work), though most users should find this
+#' level of granularity sufficient.
+#' 
+#' Loggers are hierarchical in the sense that any requested logger that is
+#' undefined will fall back to its most immediate defined parent logger. The
+#' absolute parent is ROOT, which is guaranteed to be defined for the system
+#' and cannot be deleted. This means that you can specify a new logger
+#' directly.
+#' 
+#' > flog.info("This will fall back to 'my', then 'ROOT'", name='my.logger')
+#' 
+#' You can also change the threshold or any other setting associated with a
+#' logger. This will create an explicit logger where any unspecified options
+#' are copied from the parent logger.
+#' 
+#' > flog.appender(appender.file("foo.log"), name='my')
+#' 
+#' > flog.threshold(ERROR, name='my.logger')
+#' 
+#' > flog.info("This won't print", name='my.logger') 
+#' 
+#' > flog.error("This %s print to a file", 'will', name='my.logger')
+#' 
+#' If you define a logger that you later want to remove, use flog.remove.
+#' 
+#' The option 'capture' allows you to print out more complicated data
+#' structures without a lot of ceremony. This variant doesn't accept format
+#' strings and instead appends the value to the next line of output. Consider 
+#'
+#' > m <- matrix(rnorm(12), nrow=3)
+#'
+#' > flog.info("Matrix:",m, capture=TRUE)
+#'
+#' which preserves the formatting, whereas using capture=FALSE will have 
+#' a cluttered output due to recycling.
+#' 
+#' @name flog.logger
+#' @aliases flog.remove flog.threshold flog.carp flog.trace
+#' flog.debug flog.info flog.warn flog.error flog.fatal ftry
+#' @param msg The message to log
+#' @param name The logger name to use
+#' @param capture Capture print output of variables instead of interpolate
+#' @param \dots Optional arguments to populate the format string
+#' @param expr An expression to evaluate
+#' @param finally An optional expression to evaluate at the end
+#' @return Most of these functions exist for their side effects, so there are
+#' few useful return values.
+#' @author Brian Lee Yung Rowe
+#' @keywords data
+#' @examples
+#' 
+#' flog.threshold(DEBUG)
+#' flog.debug("This debug message will print")
+#' 
+#' flog.threshold(WARN)
+#' flog.debug("This one won't")
+#' 
+#' m <- matrix(rnorm(12), nrow=3)
+#' flog.info("Matrix:",m, capture=TRUE)
+#' 
+#' ftry(log(-1))
+#' 
+NULL
+
 .log_level <- function(msg, ..., level, name, capture)
 {
   logger <- flog.logger(name)
-  if (level > logger$threshold) { return(invisible()) }
+  if (level > logger$threshold && (is.null(logger$carp) || !logger$carp)) {
+    return(invisible())
+  }
 
   appender <- flog.appender(name)
   layout <- flog.layout(name)
   if (capture) {
     values <- capture.output(print(...))
-    appender(c(layout(level, msg), values))
+    message <- c(layout(level, msg), values)
   } else {
-    appender(layout(level, msg, ...))
+    message <- layout(level, msg, ...)
   }
-  invisible()
+  if (level <= logger$threshold) appender(message)
+  invisible(message)
 }
 
 # Get the namespace that a function resides in. If no namespace exists, then
 # return NULL.
 # <environment: namespace:lambda.r>
-get_namespace() %as% 
+get_namespace(where=1) %as% 
 {
-  s <- capture.output(str(environment(sys.function(1)), give.attr=FALSE))
+  s <- capture.output(str(environment(sys.function(where)), give.attr=FALSE))
   if (length(grep('namespace', s)) < 1) return('ROOT')
 
   ns <- sub('.*namespace:([^>]+)>.*','\\1', s)
@@ -51,6 +141,12 @@ flog.fatal <- function(msg, ..., name=get_namespace(), capture=FALSE) {
   .log_level(msg, ..., level=FATAL,name=name, capture=capture)
 }
 
+ftry <- function(expr, error=stop, finally=NULL) {
+  w.handler <- function(e) flog.warn("%s", e)
+  e.handler <- function(e) { flog.error("%s", e); error(e) }
+  tryCatch(expr, warning=w.handler, error=e.handler, finally)
+}
+
 # Get a logger. By default, use the package namespace or use the 'ROOT' logger.
 flog.logger() %as%
 {
@@ -80,12 +176,13 @@ flog.logger(name) %as%
   flog.logger(parent)
 }
 
-flog.logger(name, threshold=NULL, appender=NULL, layout=NULL) %as%
+flog.logger(name, threshold=NULL, appender=NULL, layout=NULL, carp=NULL) %as%
 {
   logger <- flog.logger(name)
   if (!is.null(threshold)) logger$threshold <- threshold
   if (!is.null(appender)) logger$appender <- appender
   if (!is.null(layout)) logger$layout <- layout
+  if (!is.null(carp)) logger$carp <- carp
   
   key <- paste("logger", name, sep='.')
   logger.options(update=list(key, logger))
@@ -102,11 +199,11 @@ flog.remove(name) %as%
 }
 
 # Get the threshold for the given logger
-flog.threshold(name) %::% character : numeric
+flog.threshold(name) %::% character : character
 flog.threshold(name='ROOT') %as%
 {
   logger <- flog.logger(name)
-  logger$threshold
+  names(logger$threshold)
 }
 
 # Set the threshold
@@ -116,34 +213,21 @@ flog.threshold(threshold, name='ROOT') %as%
   invisible()
 }
 
-# Get appenders associated with the given logger
-flog.appender(name) %::% character : Function
-flog.appender(name='ROOT') %as%
+# Indicate whether the logger will always output a message
+flog.carp(name) %::% character : logical
+flog.carp(name='ROOT') %as%
 {
   logger <- flog.logger(name)
-  logger$appender
+  if (is.null(logger$carp)) FALSE
+  else logger$carp
 }
 
-# Set the appender for the given logger
-flog.appender(fn, name='ROOT') %as%
+# Set whether to carp
+flog.carp(carp, name='ROOT') %as%
 {
-  flog.logger(name, appender=fn)
+  flog.logger(name, carp=carp)
   invisible()
 }
 
-# Get the layout for the given logger
-flog.layout(name) %::% character : Function
-flog.layout(name='ROOT') %as%
-{
-  logger <- flog.logger(name)
-  logger$layout
-}
-
-# Set the layout
-flog.layout(fn, name='ROOT') %as%
-{
-  flog.logger(name, layout=fn)
-  invisible()
-}
 
 
