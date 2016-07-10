76e6d368aad7be76bca10ad6509b0919433343d7
Brian Lee Yung Rowe <r@zatonovo.com>
2016-07-10 16:57:47 +0000

    version 1.4.3

--- R/futile.logger-package.R
+++ R/futile.logger-package.R
@@ -9,8 +9,8 @@
 #' \tabular{ll}{
 #' Package: \tab futile.logger\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.4.1\cr
-#' Date: \tab 2015-04-18\cr
+#' Version: \tab 1.4.3\cr
+#' Date: \tab 2016-07-10\cr
 #' License: \tab LGPL-3\cr
 #' LazyLoad: \tab yes\cr
 #' }
@@ -90,7 +90,7 @@
 #' @aliases futile.logger-package futile.logger flog.namespace
 #' @docType package
 #' @exportPattern "^[^\\.]"
-#' @import lambda.r futile.options
+#' @import utils lambda.r futile.options
 #' @author Brian Lee Yung Rowe <r@@zatonovo.com>
 #' @seealso \code{\link{flog.logger}}, \code{\link{flog.threshold}},
 #' \code{\link{flog.layout}}, \code{\link{flog.appender}}
